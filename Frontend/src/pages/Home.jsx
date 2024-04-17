f657cb8a1425996f13e8e172be24d12e8ac0124e
TejasH MistrY <mistrytejasm@gmail.com>
2024-04-17 12:21:25 +0530

    Feature section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -9,7 +9,9 @@ import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs'
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
-
+import featureImg from '../assets/images/feature-img.png';
+import videoIcon from '../assets/images/video-icon.png'
+import avatarImg from '../assets/images/avatar-icon.png'
 
 const Home = () => {
   return (
@@ -158,6 +160,65 @@ const Home = () => {
         </div>
       </section>
       {/*==== service section ====*/}
+
+      {/*===== feature section start =====*/}
+      <section>
+        <div className='container'>
+          <div className='flex item-center justify-betweeen flex-col lg:flex-row'>
+            {/*=== feature content ====*/}
+            <div className='xl:w-[670px]'>
+              <h2 className='heading'>
+                Get viral treatment <br /> anytime.
+              </h2>
+
+              <ul className='pl-4'>
+                <li className='text_para'>
+                  1. Schedule the Appointment directly.
+                </li>
+                <li className='text_para'>
+                  2. Search for your physician here, and contact their office
+                </li>
+                <li className='text_para'>
+                  3. view our physicians who are accepting new patients, use the online secheduling tool to select an appointment time.
+                </li>
+              </ul>
+              <Link ro='/'>
+                <button className='btn'>Learn More</button>
+              </Link>
+            </div>
+
+            {/*==== feature image ==== */}
+            <div className='relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0'>
+              <img src={featureImg} className='w-3/4' alt='' />
+
+              <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
+                <div className='flex items-center justify-between'>
+                  <div className='flex item-center gap-[6px] lg:gap-3'>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
+                      Tue, 24
+                    </p>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]'>
+                      10:00Am
+                    </p>
+                  </div>
+                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                    <img src={videoIcon} alt='' />
+                  </span>
+                </div>
+
+                <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500]  mt-2 lg:mt-4 rounded-full'>
+                  Consultation
+                </div>
+              <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                <img src={avatarImg} alt="" />
+                <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>Peter Thiel</h4>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/*===== feature section end =====*/}
     </>
   )
 }
