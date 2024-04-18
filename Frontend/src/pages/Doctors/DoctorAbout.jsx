755effac754aa068b038c72a497d45ecb1daaf8f
TejasH MistrY <mistrytejasm@gmail.com>
2024-04-18 15:25:46 +0530

    feedbackform rating star

--- Frontend/src/pages/Doctors/DoctorAbout.jsx
+++ Frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -23,34 +23,71 @@ const DoctorAbout = () => {
 
         <ul className='pt-4 md:p-5'>
           <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
-            <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
-              {formateDate("09-13-2014")} - {formateDate('03-13-2016')}
-            </span>
-            <p className='text-[16px] leading-6 font-medium text-textColor'>
-              PHD in Surgeon
-            </p>
+            <div>
+              <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
+                {formateDate("09-13-2014")} - {formateDate('03-13-2016')}
+              </span>
+              <p className='text-[16px] leading-6 font-medium text-textColor'>
+                PHD in Surgeon
+              </p>
+            </div>
             <p className='text-[14px] leading-5 font-medium text-textColor'>
               New Apollo Hospital, New York
             </p>
+
           </li>
 
           <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
-            <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
-              12-02-2014
+            <div>
+              <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
+                {formateDate("09-13-2014")} - {formateDate('03-13-2016')}
+              </span>
+              <p className='text-[16px] leading-6 font-medium text-textColor'>
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className='text-[14px] leading-5 font-medium text-textColor'>
+              New Apollo Hospital, New York
+            </p>
+          </li>
+        </ul>
+      </div>
+
+
+
+      <div className='mt-12'>
+        <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
+          Experience
+        </h3>
+
+        <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
+          <li className='p-4 rounded bg-[#fff9ea]'>
+            <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+              {formateDate("09-13-2014")} - {formateDate('03-13-2016')}
             </span>
             <p className='text-[16px] leading-6 font-medium text-textColor'>
-              PHD in Surgeon
+              Sr.Surgeon
             </p>
             <p className='text-[14px] leading-5 font-medium text-textColor'>
               New Apollo Hospital, New York
             </p>
           </li>
 
+          <li className='p-4 rounded bg-[#fff9ea]'>
+            <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+              {formateDate("09-13-2014")} - {formateDate('03-13-2016')}
+            </span>
+            <p className='text-[16px] leading-6 font-medium text-textColor'>
+              Sr.Surgeon
+            </p>
+            <p className='text-[14px] leading-5 font-medium text-textColor'>
+              New Apollo Hospital, New York
+            </p>
+          </li>
         </ul>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default DoctorAbout
