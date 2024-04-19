cd244e2f942dfa33de038b6615ef86941809d08b
TejasH MistrY <mistrytejasm@gmail.com>
2024-04-19 18:48:39 +0530

    postman Api test Done

---
+++ Backend/Routes/auth.js
@@ -0,0 +1,10 @@
+// auth.js
+import express from "express";
+import { register, login } from "../Controllers/authController.js";
+
+const authRoute = express.Router();
+
+authRoute.post("/register", register);
+authRoute.post("/login", login);
+
+export default authRoute;
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoute from './Routes/auth.js';
 
-dotenv.config()
+
+dotenv.config();
 
 const app = express()
 const port = process.env.PORT || 8000
@@ -36,6 +38,7 @@ const connectDB = async()=>{
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute);  // domain/api/v1/auth/register
 
 app.listen(port, ()=>{
   connectDB();
